78930987d068b5a92ac3a5ed0ac5f9c9c55cac2c
Sukriti-09-DAA <102318754+Sukriti-09-DAA@users.noreply.github.com>
2022-05-04 16:02:51 +0530

    Add files via upload

---
+++ array_1.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums){
+        int n=nums.size();
+        vector<int> v(n,0);
+        for(int i=0;i<=n;i++){
+            if(v[nums[i]]!=0) return nums[i];
+            else v[nums[i]]++;   
+        }
+     return -1;   
+    }
+};
